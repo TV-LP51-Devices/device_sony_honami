3c9ddc7eed47a4e5f0fa32cacc399d638fd5bb86
Alx31 <alex31.couderc@gmail.com>
2015-01-07 23:28:34 +0100

    Validus-ify

---
+++ validus.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2014 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
+
+$(call inherit-product, device/sony/honami/full_honami.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6903 BUILD_FINGERPRINT=Sony/C6903/C6903:4.4.4/14.4.A.0.133/ev__jQ:user/release-keys PRIVATE_BUILD_DESC="C6903-user 4.4.4 14.4.A.0.133 ev__jQ release-keys"
+
+PRODUCT_NAME := validus_honami
+PRODUCT_DEVICE := honami
